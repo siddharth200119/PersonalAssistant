94549ec9fe3602ca4abf37d3874ac2d430f71b72
siddharth200119 <siddramnani@gmail.com>
2023-06-02 12:09:38 +0530

    prompt works now

--- assistant.py
+++ assistant.py
@@ -1,9 +1,27 @@
-import gpt4all
+import streamlit as st 
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import GPT4All
 
-gptj = gpt4all.GPT4All("ggml-mpt-7b-chat")
+PATH = "model/ggml-gpt4all-l13b-snoozy.bin"
 
-message = input("enter message: ")
-messages = [{"role": "user", "content": message}]
+llm = GPT4All(model=PATH, verbose=True)
 
+template = PromptTemplate(input_variables=['question'], template="""
+    ### Instruction: 
+    This prompt below is a question to a answer, a task to complete, or a conversation to respond to; decide which and write an appropriate response.
+    
+    ### Prompt: 
+    {question}
+    
+    ### Response:""")
 
-gptj.generate(message)
+llm_chain = LLMChain(prompt=template, llm=llm, verbose = True)
+
+st.title('🦜🔗 GPT4ALL Y\'All')
+st.info('This is using the MPT model!')
+prompt = st.text_input('Enter your prompt here!')
+
+if prompt: 
+    response = llm_chain.run(prompt)
+    print(response)
+    st.write(response)
